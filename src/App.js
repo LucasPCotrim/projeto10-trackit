f6086163a86b1e77d0f8cb31ad9d44089be23565
LucasPCotrim <lucaspcotrim@gmail.com>
2022-08-02 11:14:37 -0300

    Added jsconfig file to project root

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import GlobalStyle from './assets/styles/GlobalStyle';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import LoginPage from 'components/LoginPage/LoginPage';
+
+
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
       Hello World
     </>
   );
