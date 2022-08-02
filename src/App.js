7790fae195364eac78c3d5a80412af74cfb94efb
LucasPCotrim <lucaspcotrim@gmail.com>
2022-08-02 15:17:57 -0300

    feat: Implemented SignUpPage layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import GlobalStyle from './assets/styles/GlobalStyle';
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginPage from 'components/LoginPage/LoginPage';
+import SignUpPage from 'components/SignUpPage/SignUpPage';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/cadastro" element={<SignUpPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -1,15 +1,16 @@
-import {LoginPageWrapper, Form, SignUpLink} from './LoginPage.style'
+import {SignUpPageWrapper, Form, LoginLink} from './SignUpPage.style'
 import {useState} from "react";
 import {Link} from 'react-router-dom'
 import trackItLogo from '../../assets/imgs/trackitLogo.svg';
 
 
-
-export default function LoginPage() {
+export default function SignUpPage() {
 
   const [form, setForm] = useState({
     email: '',
     password: '',
+    name: '',
+    picture: ''
   });
 
   function handleForm(event) {
@@ -19,27 +20,31 @@ export default function LoginPage() {
     }) 
   }
 
-  function executeLogin(event){
+  function executeSignUp(event){
     event.preventDefault();
 
 		console.log(form);
 
     setForm({
       email: '',
-      password: ''
+      password: '',
+      name: '',
+      picture: ''
     }) 
 	}
 
   return (
-    <LoginPageWrapper>
+    <SignUpPageWrapper>
       <img src={trackItLogo} alt="trackItLogo" />
-      <Form onSubmit={executeLogin}>
+      <Form onSubmit={executeSignUp}>
         <input type="email" name='email' placeholder='email' value={form.email} onChange={handleForm} />
         <input type="password" name='password' placeholder='senha' value={form.password} onChange={handleForm} />
-        <button type="submit">Entrar</button>
+        <input type="text" name='name' placeholder='nome' value={form.name} onChange={handleForm} />
+        <input type="url" name='picture' placeholder='foto' value={form.picture} onChange={handleForm} />
+        <button type="submit">Cadastrar</button>
       </Form>
-      <Link to='/cadastro' ><SignUpLink>Não tem uma conta? Cadastre-se!</SignUpLink></Link>
-    </LoginPageWrapper>
+      <Link to='/' ><LoginLink>Já tem uma conta? Faça login!</LoginLink></Link>
+    </SignUpPageWrapper>
   )
 }
 
