a27060ab15d730f33c66493fdceb5e606d02c516
LucasPCotrim <lucaspcotrim@gmail.com>
2022-08-03 03:58:09 -0300

    feat: Implemented initial static layout for HistoryPage component

--- src/components/HistoryPage/HistoryPage.js
+++ src/components/HistoryPage/HistoryPage.js
@@ -1,7 +1,12 @@
-import {HistoryPageWrapper} from './HistoryPage.style'
+import {HistoryPageWrapper, PageContent} from './HistoryPage.style'
 
 export default function HistoryPage() {
   return (
-    <HistoryPageWrapper>HistoryPage</HistoryPageWrapper>
+    <HistoryPageWrapper>
+      <PageContent>
+        <h1>Histórico</h1>
+        <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+      </PageContent>
+    </HistoryPageWrapper>
   )
 }
--- src/components/HistoryPage/HistoryPage.style.js
+++ src/components/HistoryPage/HistoryPage.style.js
@@ -3,4 +3,40 @@ import styled from 'styled-components';
 export const HistoryPageWrapper = styled.div`
   background-color: #E5E5E5;
   margin-top: 70px;
+  width: 100vw;
+  min-height: calc(100vh - 70px);
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+`;
+
+export const PageContent = styled.div`
+  width: calc(min(600px, 100vw) - 34px);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 160px;
+
+  h1 {
+    margin-top: 28px;
+    margin-bottom: 17px;
+    width: 100%;
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126BA5;
+  }
+  p {
+    width: 100%;
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17.976px;
+    line-height: 22px;
+    color: #666666;
+  }
 `;
